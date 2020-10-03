60e0ee1f74ae9cd5910a9a18f0a1ada339a48b28
Wilson-La <wilsonla0920@gmail.com>
2020-10-03 17:17:29 -0400

    added urlParser method

--- HTTP-Client-Application/src/httpc.java
+++ HTTP-Client-Application/src/httpc.java
@@ -1,9 +1,38 @@
 
 public class httpc {
+	
+	private static String path;
+	private static String hostName;
+	
 	public static void main (String[] args){
 		
 	}
 	
+	public static void urlParser(String url){
+		if(url.contains("http://")){
+			url = url.substring(7);
+			if(url.contains("/")){
+				int slashIndex = url.indexOf("/");
+				hostName = url.substring(0, slashIndex);
+				path = url.substring(slashIndex);
+			}
+			else{
+				hostName = url;
+				path = "/";
+			}
+		}
+		else{
+			if(url.contains("/")){
+				int slashIndex = url.indexOf("/");
+				hostName = url.substring(0, slashIndex);
+				path = url.substring(slashIndex);
+			}
+			else{
+				hostName = url;
+				path = "/";
+			}
+		}
+	}
 	
 	//GET method
 	public static void get(String urlInput){
