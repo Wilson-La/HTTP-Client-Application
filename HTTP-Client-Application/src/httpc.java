9373f0a1c0f9ca865c4402cf5a478c994b26bb74
Wilson-La <wilsonla0920@gmail.com>
2020-10-03 16:20:43 -0400

    Added GET and POST method

--- HTTP-Client-Application/src/httpc.java
+++ HTTP-Client-Application/src/httpc.java
@@ -1,4 +1,21 @@
 
 public class httpc {
-
+	public static void main (String[] args){
+		
+	}
+	
+	public static void createMessage(){
+		
+	}
+	
+	//GET method
+	public static void get(String urlInput){
+		
+		
+		
+	}
+	//POST method
+	public static void post(){
+		
+	}
 }
