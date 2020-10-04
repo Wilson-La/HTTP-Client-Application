5cc9012f252db129c185df9a10bcfbb760239843
Wilson-La <wilsonla0920@gmail.com>
2020-10-04 14:33:49 -0400

    Finished implementing sendMsg() and get() methods.

--- HTTP-Client-Application/src/httpc.java
+++ HTTP-Client-Application/src/httpc.java
@@ -1,5 +1,12 @@
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class httpc {
 	
@@ -9,6 +16,10 @@ public class httpc {
 	private static String generalHelp;
 	private static String getHelp;
 	private static String postHelp;
+	private static Socket clientSocket = new Socket();
+	private static String port = "8080";
+	private static boolean verbose = false;
+	private static String messageToSend;
 
 	
 	public static void main (String[] args){
@@ -42,7 +53,7 @@ public class httpc {
 			}
 		}
 	}
-	
+	//createmsg() method used to create the request for either a GET or POST
 	public static String createMsg(String method, String path){
 		String msg = "";
 		Header headerArr[] = new Header[3];
@@ -71,16 +82,53 @@ public class httpc {
 		}
 		return msg;
 	}
-	//The sendMsg() method
-	public static void sendMsg(){
-		
+	//The sendMsg() method used to send request to server and also to receive the response following our request
+	public static void sendMsg(String msg){
+		try {
+			clientSocket.connect(new InetSocketAddress(hostName, Integer.parseInt(port)));
+			
+			BufferedWriter socketBWOS = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+			
+			BufferedReader socketBRIS = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			
+			//socketBWOS write's the created message
+			socketBWOS.write(msg);
+			socketBWOS.flush();
+			String response = "";
+			//the following loop is used to recieve the response
+			while ((response = socketBRIS.readLine()) != null){
+				if(response.length() == 0 && !verbose){
+					StringBuilder recieved_response = new StringBuilder();
+					while ((response = socketBRIS.readLine()) != null){
+						recieved_response.append(response).append("\r\n");
+					}
+					System.out.println(recieved_response.toString());
+					break;
+				}
+				else if(verbose){
+					System.out.println(response);
+				}
+			}
+			socketBWOS.close();
+			socketBRIS.close();
+			clientSocket.close();
+		}catch (Exception e) {
+			System.out.println("sendMsg() method has encountered an error.\n"+e.getMessage());
+		}
 	}
 	
-	//GET method
+	/*
+	GET method which uses the urlParser() to parse the url into hostName and path.
+	The method then uses the createMsg() method to create the GET request to be sent
+	Then the method finally uses the sendMsg() method to send the GET request
+	*/
 	public static void get(String urlInput){
 		
+		urlParser(urlInput);
 		
+		messageToSend = createMsg("GET", path);
 		
+		sendMsg(messageToSend);
 	}
 	//POST method
 	public static void post(String urlInput){
